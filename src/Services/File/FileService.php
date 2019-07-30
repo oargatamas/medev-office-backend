67b08ba1eac8605471de9ae8f141e5d965f5fd6c
Tangomajom <kutyafika>
2019-07-30 11:11:24 +0200

    Routes params fixed to log to the correct service log.

--- src/Services/File/FileService.php
+++ src/Services/File/FileService.php
@@ -12,6 +12,7 @@ namespace MedevOffice\Services\File;
 use MedevOffice\Services\File\Actions\Api\Folder\DownloadFile;
 use MedevOffice\Services\File\Actions\Api\Folder\GetFolderContent;
 use MedevOffice\Services\File\Actions\Api\Folder\UploadFileToFolder;
+use MedevSlim\Core\Application\MedevApp;
 use MedevSlim\Core\Service\APIService;
 use Slim\App;
 
@@ -19,6 +20,16 @@ class FileService extends APIService
 {
     const FOLDER_ID = "folderId";
     const FILE_ID = "file_id";
+
+    const ROUTE_DOWNLOAD_FILE = "downloadFile";
+    const ROUTE_GET_FOLDER_CONTENT = "getfolderContent";
+    const ROUTE_UPLOAD_FILE = "uploadFile";
+
+
+    public function __construct(MedevApp $app)
+    {
+        parent::__construct($app);
+    }
 
     /**
      * @return mixed
@@ -35,12 +46,15 @@ class FileService extends APIService
     protected function registerRoutes(App $app)
     {
         $app->get("/file/{".self::FILE_ID."}/data", new DownloadFile($this))
-            ->setName($this->getServiceName());
+            ->setArgument(APIService::SERVICE_ID,$this->getServiceName())
+            ->setName(self::ROUTE_DOWNLOAD_FILE);
 
         $app->get("/folder/{".self::FOLDER_ID."}/content", new GetFolderContent($this))
-            ->setName($this->getServiceName());
+            ->setArgument(APIService::SERVICE_ID,$this->getServiceName())
+            ->setName(self::ROUTE_GET_FOLDER_CONTENT);
 
         $app->post("/folder/{".self::FOLDER_ID."}/file", new UploadFileToFolder($this))
-            ->setName($this->getServiceName());
+            ->setArgument(APIService::SERVICE_ID,$this->getServiceName())
+            ->setName(self::ROUTE_UPLOAD_FILE);
     }
 }
