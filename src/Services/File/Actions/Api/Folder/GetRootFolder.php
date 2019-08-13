ec52ea033d49e3a18196099438f79baf525e36f2
Tangomajom <kutyafika>
2019-08-13 14:55:46 +0200

    withMeta option has been added to the root folder query action.

--- src/Services/File/Actions/Api/Folder/GetRootFolder.php
+++ src/Services/File/Actions/Api/Folder/GetRootFolder.php
@@ -9,6 +9,9 @@
 namespace MedevOffice\Services\File\Actions\Api\Folder;
 
 
+use MedevAuth\Services\Auth\OAuth\Entity\Token\OAuthToken;
+use MedevAuth\Services\Auth\OAuth\OAuthService;
+use MedevOffice\Services\File\Actions\Repository\Folder\GetFolderMeta;
 use MedevOffice\Services\File\Actions\Repository\Folder\GetRootFolderId;
 use MedevSlim\Core\Action\Servlet\APIServlet;
 use Slim\Http\Request;
@@ -26,12 +29,26 @@ class GetRootFolder extends APIServlet
      */
     public function handleRequest(Request $request, Response $response, $args)
     {
+        /** @var OAuthToken $authToken */
+        $authToken = $request->getAttribute(OAuthService::AUTH_TOKEN);
+        $withMeta = $request->getParam("meta",false);
+
         $getRootFolderId = new GetRootFolderId($this->service);
         $rootFolderId = $getRootFolderId->handleRequest();
 
         $data = [
             "id" => $rootFolderId
         ];
+
+        if($withMeta){
+            $getFolderMeta = new GetFolderMeta($this->service);
+            $folderInfo = $getFolderMeta->handleRequest([
+                GetFolderMeta::FOLDER_ID => $rootFolderId,
+                GetFolderMeta::REQUESTER => $authToken->getUser()->getIdentifier()
+            ]);
+
+            $data = $folderInfo;
+        }
 
         return $response->withJson($data,200);
     }
