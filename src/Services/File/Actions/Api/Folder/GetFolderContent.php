54d02574aa881c3c39d9da8787c7f3657edebbc5
Tangomajom <kutyafika>
2019-08-15 16:29:36 +0200

    folder meta query pushed back to get folder content. It's simply needed for the client

--- src/Services/File/Actions/Api/Folder/GetFolderContent.php
+++ src/Services/File/Actions/Api/Folder/GetFolderContent.php
@@ -13,6 +13,7 @@ namespace MedevOffice\Services\File\Actions\Api\Folder;
 use MedevAuth\Services\Auth\OAuth\Entity\Token\OAuthToken;
 use MedevAuth\Services\Auth\OAuth\OAuthService;
 use MedevOffice\Services\File\Actions\Repository\Folder\GetFolderItems;
+use MedevOffice\Services\File\Actions\Repository\Folder\GetFolderMeta;
 use MedevOffice\Services\File\Actions\Repository\GetAllParentOfItem;
 use MedevOffice\Services\File\Entities\Permission;
 use MedevOffice\Services\File\Middleware\PermissionRestricted;
@@ -39,6 +40,12 @@ class GetFolderContent extends APIServlet implements PermissionRestricted
         $userId = $authToken->getUser()->getIdentifier();
         $folderId = $args[OfficeFileService::FOLDER_ID];
 
+        $getFolder = new GetFolderMeta($this->service);
+        $folder = $getFolder->handleRequest([
+            GetFolderMeta::FOLDER_ID => $folderId,
+            GetFolderMeta::REQUESTER => $userId,
+        ]);
+
         $getItemParents = new GetAllParentOfItem($this->service);
         $parents = $getItemParents->handleRequest([
             GetAllParentOfItem::ITEM_ID => $folderId,
@@ -52,6 +59,7 @@ class GetFolderContent extends APIServlet implements PermissionRestricted
         ]);
 
         $data = [
+            "meta" => $folder,
             "parents" => $parents,
             "content" => $items,
         ];
